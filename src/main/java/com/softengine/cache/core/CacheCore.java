1ca7814b45722c4a522f05a6e61073b8c44f4019
mhmtonrn <mhmtonar1@gmail.com>
2022-10-11 21:55:20 +0300

    Initial commit

---
+++ src/main/java/com/softengine/cache/core/CacheCore.java
@@ -0,0 +1,29 @@
+package com.softengine.cache.core;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * cache core class
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CacheCore {
+    private static Map<String,Object> cache = new ConcurrentHashMap<>();
+
+    public static  <T> T get(String key, Class<T> tClass){
+        return tClass.cast(cache.get(key));
+    }
+
+    public static <T> void put(String key, T value){
+        cache.put(key,value);
+    }
+
+    public static void remove(String key){
+        System.out.println("removed "+key);
+        cache.remove(key);
+    }
+}
